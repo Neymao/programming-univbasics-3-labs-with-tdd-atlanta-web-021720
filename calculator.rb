5c2a049fdad55bef42d6e174c4d7044c8053182e
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-01-11 20:42:04 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,3 +5,4 @@ expect(second_number).not_to equal(0)
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
+quotient = first_number / second_number
